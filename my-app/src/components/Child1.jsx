1a40454b733d206f4ddf2def4eb4e8bb53c660ef
yuhyeongeun-kor <63186208+YuHyeonGeun-KOR@users.noreply.github.com>
2024-01-08 06:50:31 +0000

    Update Child1.jsx

--- my-app/src/components/Child1.jsx
+++ my-app/src/components/Child1.jsx
@@ -1,19 +1,20 @@
-import { Child2 } from "./Child2"
-import { Child3 } from "./Child3"
+import { memo } from "react";
+import { Child2 } from "./Child2";
+import { Child3 } from "./Child3";
 
 const style = {
-    height: "200px",
-    backgroundColor: "lightblue",
-    padding: "8px"
+  height: "200px",
+  backgroundColor: "lightblue",
+  padding: "8px",
 };
 
-export const Child1 = () => {
-    console.log("Child1 렌더링");
-    return (
-        <div style={style}>
-            <p>Child1</p>
-            <Child2/>
-            <Child3/>
-        </div>
-    )
-}
+export const Child1 = memo(() => {
+  console.log("Child1 렌더링");
+  return (
+    <div style={style}>
+      <p>Child1</p>
+      <Child2 />
+      <Child3 />
+    </div>
+  );
+});
